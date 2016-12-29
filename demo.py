2e21197f365d3fb9d763eaadb47d6fb4b5f4dafa
ASCI-Red <erika.h.2012@gmail.com>
2016-12-29 10:57:29 -0800

    added some python

---
+++ demo.py
@@ -0,0 +1,26 @@
+
+
+
+
+import flask, flask.views
+import os
+
+
+app = flask.Flask(__name__)
+app.secret_key="Jaffa"
+
+class View(flask.views.MethodView):
+	def get(self):
+		return flask.render_template('index.html')
+
+	def post(self):
+		#return flask.request.form['expression']
+		result = eval(flask.request.form['expression'])
+		flask.flash(result)
+		return self.get()
+
+app.add_url_rule('/', view_func=View.as_view('main'), methods = ['GET', 'POST'])
+
+app.debug = True
+
+app.run()
