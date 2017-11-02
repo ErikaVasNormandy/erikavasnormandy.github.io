1bc69bf0708b68f95bde487387ea6568cc355156
ASCI-Red <erika.h.2012@gmail.com>
2017-11-02 16:23:19 -0700

    Added SickOS

--- demo.py
+++ demo.py
@@ -1,26 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
 
-import flask, flask.views
-import os
+@app.route('/')
+def index():
+	return render_template("index.html")
 
+@app.route('/')
+def sickOS():
+	return render_template("/sickOS/sickOS.html")
 
-app = flask.Flask(__name__)
-app.secret_key="Jaffa"
-
-class View(flask.views.MethodView):
-	def get(self):
-		return flask.render_template('index.html')
-
-	def get('/sickOS'):
-		return flask.render_template('sickOS/sickOS.html')
-
-	def post(self):
-		#return flask.request.form['expression']
-		result = eval(flask.request.form['expression'])
-		flask.flash(result)
-		return self.get()
-
-app.add_url_rule('/', view_func=View.as_view('main'), methods = ['GET', 'POST'])
-
-app.debug = True
-
-app.run()
+if __name__ == '__main__':
+	app.run(debug=True);
